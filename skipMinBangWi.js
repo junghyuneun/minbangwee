975595873cf2a23fd56987b6cddf862747ae9640
junghyuneun <junghyun.eun@gmail.com>
2025-07-03 21:10:17 +0900

    2025 version

--- skipMinBangWi.js
+++ skipMinBangWi.js
@@ -2,12 +2,10 @@ const mem_no = document.getElementsByName('mem_no')?.[0]?.value || '0000000' // 
 const cookie = "" // fill in as needed. example value: "ch-veil-id=a4c5c458-fe5f-430d-bfa7-b257bd461227; ch-session-67536=eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJzZXMiLCJrZXkiOiI2NzUzNi02ODY2NGE0Y2YxY2NiZTNmNWE3ZSIsImlhdCI6MTc1MTUzNDUxOSwiZXhwIjoxNzU0MTI2NTE5fQ.DKJMygLG1XxaJEzeF_yWl0u1Ksi0tcA5bf-hC7aW9_M; connect.sid=s%3AZOPlHW0GS29mzgJB589FsDn-Xse2UOdo.ZRi8wjDQbg9em2X8ZAEwmRwQ7ghuwHWWlRavt7qvZgs"
 const mov_order = document.getElementsByName("mov_order")?.[0]?.value || 0
 
-const skipToEnd = (videoObject, timeToSkip) => {
+const skipTo = (videoObject, timeToSkip) => {
   // try and avoid pauses after seeking
   videoObject.pause()
-  videoObject.currentTime = timeToSkip // if this is far enough away from current, it implies a "play" call as well...oddly. I mean seriously that is junk.
-  // however if it close enough, then we need to call play manually
-  // some shenanigans to try and work around this:
+  videoObject.currentTime = timeToSkip
   var timer = setInterval(function () {
     if (videoObject.readyState == 4 || !videoObject.paused) {
       videoObject.play()
@@ -20,7 +18,7 @@ const skip = async () => {
   let vidoeObject = document.getElementsByTagName('video')[0]
   let duration = vidoeObject.duration
   let skipToTarget = duration - 1
-  skipToEnd(vidoeObject, skipToTarget)
+  skipTo(vidoeObject, skipToTarget)
   await sendAjax(skipToTarget)
   document.getElementsByName("btnclose")[0].click()
 }
